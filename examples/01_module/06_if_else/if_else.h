1729ab08e1df6db73f55e4b9cc2cf90c6b8a25ff
k1788723 <karen.camposhenry@g.austincc.edu>
2019-01-31 19:18:01 -0600

    Conditional Examples

--- examples/01_module/06_if_else/if_else.h
+++ examples/01_module/06_if_else/if_else.h
@@ -1,4 +1,5 @@
 //write prototype for function named get_generation that accepts an int and
 //returns a string
+#include<string>
 
-
+string::std get_generation(int year);
--- examples/01_module_test/01_module_test.cpp
+++ examples/01_module_test/01_module_test.cpp
@@ -1,15 +1,26 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "output.h"
+#include "if.h"
+#include "if_else.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
+TEST_CASE("Verify Test Configuration", "verification"){
 	REQUIRE(true == true);
 }
 
 //Create a test case for the if statement using:
 //the number 4 result should be true
 //the number 55 result should be false
-
+TEST_CASE("Test is even")
+{
+	REQUIRE(is_even(4) == true);
+	REQUIRE(is_even(55) == false);
+}
+TEST_CASE("Test overtime function")
+{
+	REQUIRE(overtime(41) == true);
+	REQUIRE(overtime(35)== true);
+}
 //Create a test case for the if else statement using:
 //the year 1930 result should be 'Silent Generation'
 //the year 1946 result should be 'Baby boomer'
@@ -18,7 +29,16 @@ TEST_CASE("Verify Test Configuration", "verification") {
 //the year 1996 result should be 'Centennial'
 //the year 1900 result should be 'Invalid Year'
 //the year 2019 result should be 'Invalid Year'
-
+TEST_CASE("Test get_generation function")
+{
+	REQUIRE(get_generation(1930) == "silent Generation");
+	REQUIRE(get_generation(1946) == "Baby boomer");
+	REQUIRE(get_generation(1970) == "Generation x");
+	REQUIRE(get_generation(1995) == "Millenial");
+	REQUIRE(get_generation(1996) == "Centennial");
+	REQUIRE(get_generation(1900) == "Invalid year");
+	REQUIRE(get_generation(2019) == "Invalid year");
+}
 
 //Create a test case for the switch statement using:
 //value 1 result should be 'Option 1'
