404324f59e2ec5c35c9bf1414537b2d08cbd5bcd
k1788723 <karen.camposhenry@g.austincc.edu>
2019-02-21 17:50:13 -0600

    homework4

--- homework/04_vectors/vectors.cpp
+++ homework/04_vectors/vectors.cpp
@@ -1,3 +1,4 @@
+#include "vectors.h"
 /*
 Write a value return function get_max_from_vector with a const reference 
 vector of intsparameter that returns the max value in a vector
@@ -5,6 +6,12 @@ vector of intsparameter that returns the max value in a vector
 @param numbers is a const reference to a vector of integers
 @return the max value in the vector
 */
+int get_max_from_vector(const std::vector<int>& number)
+{
+
+	return 0;
+}
+
 
 
 
@@ -15,6 +22,11 @@ given a number returns true if prime or false if not prime
 @param number: Any whole number
 @return: bool if prime False if not
 */
+bool is_prime(int number)
+{
+	return false;
+}
+
 
 /*
 Write a a function named vector_of_primes with an integer parameter
@@ -28,3 +40,10 @@ Example given number 10 returns a vector with elements 2,3,5,7,
 Make sure to use the is_prime function to determine if current 
 number is prime.
 */
+
+vector<int> vector_of_primes(int number);
+{
+	return 0;
+}
+
+
--- homework/04_vectors_test/vectors_test.cpp
+++ homework/04_vectors_test/vectors_test.cpp
@@ -13,7 +13,22 @@ vector values 15,12,11,99,88 returns 99
 vector values 150,120,11,990,88888 returns 88888
 */
 
+TEST_CASE("max")
+{
+	REQUIRE(get_max_from_vector(3, 8, 1, 99, 1000) == 1000);
+	REQUIRE(get_max_from_vector(15, 12, 11, 99, 88) == 99);
+	REQUIRE(get_max_from_vector(150, 120, 11, 990, 88888) == 88888);
+}
 
+TEST_CASE("Test pass vector by reference")
+{
+	std::vector<int> num = (1000, 55, 77, 11, 99);
+	std::vector<int> nums = (44, 55, 77, 11, 99);
+	loop_vector(nums);
+	for int i = 0; i < num.size(); ++1;
+	{
+		REQUIRE(num[i] == nums[i]);
+	}
 /*
 Write a test case for is_prime with values:
 2 returns true
@@ -22,7 +37,13 @@ Write a test case for is_prime with values:
 44 returns false
 */
 
-
+TEST_CASE("prime test") 
+{
+	REQUIRE(is_prime(2) == true);
+	REQUIRE(is_prime(4) == false);
+	REQUIRE(is_prime(43) == true);
+	REQUIRE(is_prime(44) == false);
+}
 /*
 Write test case for vector_of_primes here with
 argument 10 return vector should have the following items:
@@ -30,3 +51,9 @@ argument 10 return vector should have the following items:
 argument 50 return vector should have the following items:
 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47
 */
+TEST_CASE("vector test")
+{
+	REQUIRE(vector_of_primes(10) == 2 3 5 7);
+	REQUIRE(vector_of_primes(50) == 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47);
+	
+}
