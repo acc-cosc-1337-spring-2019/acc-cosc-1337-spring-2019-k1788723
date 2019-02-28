df8fe10048aef0399a9902d8934139afb21c3b86
k1788723 <karen.camposhenry@g.austincc.edu>
2019-02-28 00:00:48 -0600

    tic tac toe

--- homework/tic_tac_toe/main.cpp
+++ homework/tic_tac_toe/main.cpp
@@ -1,4 +1,14 @@
+#include "tic_tac_toe.h"
+#include <iostream>
+
 int main() 
 {
+	TicTacToe TicTacToe();
+	TicTacToe.start_game(std::string first_player);
+	
+	std::cout << "Player one: choose X or O";
+	std::cin >> first_player;
+	
+
 	return 0;
 }
--- homework/tic_tac_toe/tic_tac_toe.cpp
+++ homework/tic_tac_toe/tic_tac_toe.cpp
@@ -1,8 +1,10 @@
 #include "tic_tac_toe.h"
+#include<iostream>
 
 void TicTacToe::start_game(std::string first_player)
 {
 	next_player = first_player;
+	clear_board();
 }
 
 std::string TicTacToe::get_player() const
@@ -10,9 +12,9 @@ std::string TicTacToe::get_player() const
 	return next_player;
 }
 
-bool TicTacToe::game_over() //
+bool TicTacToe::game_over() 
 {
-	if (check_column_win() || check_row_win() || check_diagonal_win());
+	if (check_column_win() || check_row_win() || check_diagonal_win())
 	{
 		return true;
 	}
@@ -21,11 +23,11 @@ bool TicTacToe::game_over() //
 
 void TicTacToe::mark_board(int position)
 {
-	pegs[position - 1] = next_player; //
+	pegs[position - 1] = next_player; 
 	set_next_player();
 }
 
-void TicTacToe::set_next_player()   //
+void TicTacToe::set_next_player()   
 {
 	if(next_player == "X")
 	{
@@ -54,9 +56,65 @@ bool TicTacToe::check_column_win()
 	}
 	return false;
 }
+
 /*
 win by row if 
 0,1,2 are equal
 3,4,5
 6,7,8
 */
+bool TicTacToe::check_row_win()
+{
+	if (pegs[0] == pegs[1] && pegs[1] == pegs[2] && pegs[2] != " ")
+	{
+		return true;
+	}
+	else if (pegs[3] == pegs[4] && pegs[4] == pegs[5] && pegs[5] != " ")
+	{
+		return true;
+	}
+	else if (pegs[6] == pegs[7] && pegs[7] == pegs[8] && pegs[8] != " ")
+	{
+		return true;
+	}
+
+	return false;
+}
+
+bool TicTacToe::check_diagonal_win()
+{
+	if (pegs[0] == pegs[4] && pegs[4] == pegs[8] && pegs[8] != " ")
+	{
+		return true;
+	}
+	else if (pegs[2] == pegs[4] && pegs[4] == pegs[6] && pegs[6] != " ")
+	{
+		return true;
+	}
+
+	return false;
+}
+
+bool TicTacToe::check_board_full()
+{
+	for (std::size_t i = 0; i <= 9; ++i)
+	{
+		if (pegs[i] != " ")
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+void TicTacToe::clear_board()
+{
+	for (auto i : pegs)
+	{
+		i = " ";
+	}
+}
+void TicTacToe::display_board() const
+{
+	std::cout << mark_board();
+}
